5f1dbc207aa11ad1e186c2054d6c03a6a73f18ff
Jhansi Rani <jhansi@Jhansis-MacBook-Air.local>
2021-06-13 17:19:04 -0400

    generators

--- main.js
+++ main.js
@@ -70,3 +70,32 @@ Promise.race([promise1, promise3,promise2]).then(function(value) {
   console.log(value);
 
 });
+
+
+
+// generator functions with promises
+/* Using a generator in combination with a Promise will allow us to write asynchronous code that feels like synchronous code.
+What we want to do is wait for a promise to resolve and then pass the resolved value back into our generator in the .next() call.
+ */
+const myPromise = () => new Promise((resolve) => {
+    resolve("our value is...");
+});
+
+function* gen() {
+    let result = "";
+    // returns promise
+    yield myPromise().then(data => { result = data }) ;
+    // wait for the promise and use its value
+    yield result + 'john is under custody';
+};
+
+// Call the async function and pass params
+const asyncFunc = gen();
+const val1 = asyncFunc.next();
+console.log(val1);
+// call the promise and wait for it to resolve
+// {value: Promise, done: false}
+val1.value.then(() => {
+    console.log(asyncFunc.next());
+})
+// Object { value: "our value is... 2", done: false }

9e3ff0e4c1f31e38deeb4b12346edbc116261e9d
Jhansi Rani <jhansi@Jhansis-MacBook-Air.local>
2021-06-14 18:38:03 -0400

    new list added

--- hideandseek.js
+++ hideandseek.js
@@ -52,3 +52,20 @@ ulcontent.appendChild(liItem3);
 // styling list items
 ulcontent.style.color = "darkcyan";
 ulcontent.setAttribute('type','I');
+
+// js to add new list on web
+var reset = document.createElement('input');
+var resetbtn = document.querySelector('body');
+resetbtn.appendChild(reset);
+reset.setAttribute("type","reset");
+reset.setAttribute("value","reset the input")
+
+var todoList=document.getElementById('todoList');
+var additems=document.getElementById('addTodo');
+
+additems.onclick=function(){
+    var li = document.createElement('li');
+    li.innerHTML=document.getElementById("todo").value;
+    todoList.appendChild(li);
+    document.getElementById("todo").value=" ";
+}
